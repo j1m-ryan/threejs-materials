e16d86e3598fb8f31330e805f2e740862476c588
Jim Ryan <mail@jimryan.ie>
2023-12-25 21:51:37 +0000

    initial

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,56 @@ import { Timer } from "three/examples/jsm/misc/Timer.js";
 import { resizeRendererToDisplaySize } from "./utils/resize";
 import GUI from "lil-gui";
 import _fullscreen from "./utils/fullscreen";
-import gsap from "gsap";
+import { RGBELoader } from "three/examples/jsm/Addons.js";
 
 const gui = new GUI({
   title: "ThreeJS Starter Bun",
 });
 
-const cubeFolder = gui.addFolder("cube");
-const cameraFolder = gui.addFolder("camera");
-
-const cubeProperties = {
-  color: "#ff0000",
-  subdivisions: 20,
-  animation: () => {},
+const loadingManager = new THREE.LoadingManager();
+loadingManager.onStart = () => {
+  console.log("started loading");
 };
+loadingManager.onLoad = () => {
+  console.log("finished loading");
+};
+loadingManager.onProgress = () => {
+  console.log("loading");
+};
+loadingManager.onError = () => {
+  console.log("error loading");
+};
+const textureLoader = new THREE.TextureLoader(loadingManager);
+const alphaTexutre = textureLoader.load("../static/textures/door/alpha.jpg");
+const ambientOcclusion = textureLoader.load(
+  "../static/textures/door/ambientOcclusion.jpg"
+);
+const colorTexture = textureLoader.load("../static/textures/door/color.jpg");
+const heightTexture = textureLoader.load("../static/textures/door/height.jpg");
+const metalnessTexture = textureLoader.load(
+  "../static/textures/door/metalness.jpg"
+);
+const normalTexture = textureLoader.load("../static/textures/door/normal.jpg");
+const roughnessTexture = textureLoader.load(
+  "../static/textures/door/roughness.jpg"
+);
+
+const gradient3Texture = textureLoader.load(
+  "../static/textures/gradients/3.jpg"
+);
+const gradient5Texture = textureLoader.load(
+  "../static/textures/gradients/5.jpg"
+);
+const matcap1Texture = textureLoader.load("../static/textures/matcaps/1.png");
+
+colorTexture.colorSpace = THREE.SRGBColorSpace;
+matcap1Texture.colorSpace = THREE.SRGBColorSpace;
+gradient3Texture.minFilter = THREE.NearestFilter;
+gradient3Texture.magFilter = THREE.NearestFilter;
+gradient3Texture.generateMipmaps = false;
+
+const cameraFolder = gui.addFolder("camera");
+const materialFolder = gui.addFolder("material");
 
 function main() {
   const canvas = document.getElementById("c");
@@ -28,7 +64,7 @@ function main() {
 
   const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
-  const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
+  const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 100);
   camera.position.z = 2;
   cameraFolder
     .add(camera.position, "z")
@@ -39,69 +75,105 @@ function main() {
 
   const scene = new THREE.Scene();
 
+  const rgbeLoader = new RGBELoader(loadingManager);
+  rgbeLoader.load("../static/textures/environmentMap/2k.hdr", (envmap) => {
+    envmap.mapping = THREE.EquirectangularReflectionMapping;
+    scene.background = envmap;
+    scene.environment = envmap;
+  });
+
   const controls = new OrbitControls(camera, canvas);
   controls.enableDamping = true;
 
-  const material = new THREE.MeshPhongMaterial({
-    color: cubeProperties.color,
-  });
-
-  cubeFolder.add(material, "wireframe");
-
-  cubeFolder.addColor(cubeProperties, "color").onChange(() => {
-    material.color.set(cubeProperties.color);
-  });
-
-  const geometry = new THREE.BoxGeometry(
-    1,
-    1,
-    1,
-    cubeProperties.subdivisions,
-    cubeProperties.subdivisions,
-    cubeProperties.subdivisions
-  );
-
-  const cube = new THREE.Mesh(geometry, material);
-  cubeFolder.add(cube.position, "y").min(-3).max(3).step(0.1).name("elevation");
-
-  cubeFolder
-    .add(cubeProperties, "subdivisions")
-    .min(1)
-    .max(20)
-    .step(1)
-    .onFinishChange(() => {
-      cube.geometry.dispose();
-      cube.geometry = new THREE.BoxGeometry(
-        1,
-        1,
-        1,
-        cubeProperties.subdivisions,
-        cubeProperties.subdivisions,
-        cubeProperties.subdivisions
-      );
-    });
-
-  cubeProperties.animation = () => {
-    gsap.to(cube.rotation, { x: cube.rotation.x + Math.PI });
-  };
-  cubeFolder.add(cubeProperties, "animation").name("roll");
-
-  cubeFolder.add(cube, "visible");
-
-  scene.add(cube);
-
   const color = 0xffffff;
-  const intensity = 3;
-  const light = new THREE.DirectionalLight(color, intensity);
-  light.position.set(-1, 2, 4);
+  const intensity = 50;
+  const light = new THREE.PointLight(color, intensity);
+  light.position.set(-1, 5, 4);
   scene.add(light);
+
+  // const material = new THREE.MeshStandardMaterial();
+  // material.side = THREE.DoubleSide;
+  // material.metalness = 1;
+  // material.roughness = 1;
+  // material.map = colorTexture;
+  // material.aoMap = ambientOcclusion;
+  // material.displacementMap = heightTexture;
+  // // material.aoMapIntensity = 1;
+  // materialFolder.add(material, "metalness").min(0).max(1).step(0.01);
+  // // materialFolder.add(material, "roughness").min(0).max(1).step(0.01);
+  // material.displacementScale = 0.1;
+  // // material.metalnessMap = metalnessTexture;
+  // material.normalMap = normalTexture;
+  // material.transparent = true;
+  // material.alphaMap = alphaTexutre;
+
+  const material = new THREE.MeshPhysicalMaterial();
+  material.side = THREE.DoubleSide;
+  material.metalness = 1;
+  material.roughness = 1;
+  material.map = colorTexture;
+  material.aoMap = ambientOcclusion;
+  material.displacementMap = heightTexture;
+  material.aoMapIntensity = 1;
+  materialFolder.add(material, "metalness").min(0).max(1).step(0.01);
+  materialFolder.add(material, "roughness").min(0).max(1).step(0.01);
+  material.displacementScale = 0.1;
+  material.metalnessMap = metalnessTexture;
+  material.normalMap = normalTexture;
+  material.transparent = true;
+  material.alphaMap = alphaTexutre;
+  // material.clearcoat = 1;
+  // material.clearcoatRoughness = 0;
+  // material.sheen = 1;
+  // material.sheenRoughness = 0.25;
+  // material.sheenColor.set(1, 1, 1);
+  // material.iridescence = 1;
+  // material.iridescenceIOR = 1;
+  // material.iridescenceThicknessRange = [100, 800];
+  material.transmission = 1;
+  material.ior = 1.5;
+  material.thickness = 0.5;
+
+  materialFolder.add(material, "transmission").min(0).max(1);
+  materialFolder.add(material, "ior").min(1).max(10);
+  materialFolder.add(material, "thickness").min(0).max(1);
+
+  // materialFolder.add(material, "iridescenceIOR").min(0).max(1);
+
+  // material.gradientMap = gradient3Texture;
+
+  // material.flatShading = true;
+  // material.matcap = matcap1Texture;
+
+  // Sphere
+  const sphereGeometry = new THREE.SphereGeometry(0.5, 16, 16);
+  const sphere = new THREE.Mesh(sphereGeometry, material);
+  sphere.position.x = -2;
+  scene.add(sphere);
+
+  // Plane
+  const planeGeometry = new THREE.PlaneGeometry(1, 1, 100, 100);
+  const plane = new THREE.Mesh(planeGeometry, material);
+  scene.add(plane);
+
+  // Torus
+  const torusGeometry = new THREE.TorusGeometry(0.3, 0.2, 16, 32);
+  const torus = new THREE.Mesh(torusGeometry, material);
+  torus.position.x = 2;
+  scene.add(torus);
 
   const timer = new Timer();
 
   const tick = () => {
     const elapsedTime = timer.getElapsed();
     timer.update();
-    cube.rotation.y = elapsedTime * 0.5;
+    sphere.rotation.x = elapsedTime;
+    plane.rotation.x = elapsedTime;
+    torus.rotation.x = elapsedTime;
+
+    sphere.rotation.y = -elapsedTime;
+    plane.rotation.y = -elapsedTime;
+    torus.rotation.y = -elapsedTime;
 
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
